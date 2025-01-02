6f7c342e01d9fca695eb9cb34c03c0d20bed3ba4
Techchauhan <rishabkumarchauhan@gmail.com>
2025-01-02 21:05:32 +0530

    show the video and fix the product by tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CategoriesDisplay from "@/components/Section/Categories/page"
 import DisplayProductByTags from "@/components/Section/ProductByTags/page"
+import ProductByVideos from "@/components/Section/ProductByVideos/page"
 
 export default function Home() {
   return (
    <div>
       <CategoriesDisplay/>
       <DisplayProductByTags/>
+      <ProductByVideos/>
       <h1>Hello</h1>
    </div>
   );
