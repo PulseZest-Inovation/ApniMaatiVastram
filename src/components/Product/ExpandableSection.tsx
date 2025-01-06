656ed28fdd8096638eb99574304170034cbc7600
Techchauhan <rishabkumarchauhan@gmail.com>
2025-01-06 20:29:37 +0530

    update the ui

--- src/components/Product/ExpandableSection.tsx
+++ src/components/Product/ExpandableSection.tsx
@@ -15,7 +15,7 @@ const ExpandableSection: React.FC<ExpandableSectionProps> = ({ title, content })
         className="cursor-pointer border-b py-2 flex justify-between items-center"
         onClick={() => setIsExpanded(!isExpanded)}
       >
-        <h2 className="text-xl font-bold">{title}</h2>
+        <h2 className="text-xl font-bold uppercase">{title}</h2>
         {isExpanded ? <FaCaretUp /> : <FaCaretDown />}
       </div>
       {isExpanded && <div className="p-4 bg-gray-100 rounded-lg shadow-md">{content}</div>}
--- src/components/Product/ProductDetails.tsx
+++ src/components/Product/ProductDetails.tsx
@@ -1,66 +1,115 @@
 import React from "react";
-import { Button } from "antd";
-import { ShoppingCartOutlined, CheckCircleOutlined } from "@ant-design/icons";
+import { Button } from "@nextui-org/react";
+import { ShoppingCartOutlined } from "@ant-design/icons";
 import { ProductType } from "@/Types/data/ProductType";
+import { HeartTwoTone } from "@ant-design/icons";
 
 interface ProductDetailsProps {
   product: ProductType;
 }
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => (
-  <div className=" space-y-6">
-    {/* Product Title */}
-    <h1
-      className="text-2xl font-bold text-gray-800 capitalize break-words"
-      style={{
-        wordBreak: "break-word",
-        whiteSpace: "normal",
-        lineHeight: "1.2",
-      }}
-    >
-      {product.productTitle}
-    </h1>
-    <h1
-      className="text-1xl font-bold text-gray-800 capitalize break-words"
-      style={{
-        wordBreak: "break-word",
-        whiteSpace: "normal",
-        lineHeight: "1.2",
-      }}
-    >
-      {product.productSubtitle}
-    </h1>
+  <div className="relative flex flex-col md:space-y-2">
 
-    {/* Price Section */}
-    <div className="flex items-center space-x-4">
-      {product.regularPrice && (
-        <span className="text-xl text-gray-500 line-through">
-          ₹{product.regularPrice}
-        </span>
-      )}
-      <span className="text-3xl font-bold text-gray-700">₹{product.price}</span>
-    </div>
-
-    {/* Short Description */}
-    <p className="text-gray-700">{product.shortDescription}</p>
-
-    {/* Action Buttons */}
-    <div className="mt-4 flex space-x-4">
-      <Button
-        icon={<ShoppingCartOutlined />}
-        className="px-6 py-2 bg-blue-600 text-white"
-        style={{ border: "none", borderRadius: "4px" }}
+      {/* Product  title */}
+      <h1
+        className="text-2xl font-normal text-gray-500 capitalize break-words"
+        style={{
+          wordBreak: "break-word",
+          whiteSpace: "normal",
+          lineHeight: "1.2",
+        }}
       >
-        Add to Cart
-      </Button>
-      <Button
-        icon={<CheckCircleOutlined />}
-        className="px-6 py-2 bg-green-600 text-white"
-        style={{ border: "none", borderRadius: "4px" }}
+        {product.productTitle}
+      </h1>
+
+      {/* Product subtitle */}
+      <h2
+        className="text-1xl text-gray-500 capitalize break-words font-thin mt-2"
+        style={{
+          wordBreak: "break-word",
+          whiteSpace: "normal",
+          lineHeight: "1.2",
+        }}
       >
-        Buy Now
-      </Button>
-    </div>
+        {product.productSubtitle}
+      </h2>
+
+   
+     
+      
+
+
+      {/* Price Section */}
+      <div className="mt-2 mb-2">
+      <div className="flex items-center space-x-4">
+        {product.regularPrice && (
+          <span className="text-xl text-gray-500 line-through">
+            ₹{product.regularPrice}
+          </span>
+        )}
+        <span className="text-3xl font-bold text-gray-700">₹{product.price}</span>
+      
+      </div>
+      <p
+        className="text-1xl text-gray-500 capitalize break-words font-thin"
+        style={{
+          wordBreak: "break-word",
+          whiteSpace: "normal",
+        }}
+      >
+        Inclusive of All Taxes
+      </p>
+      </div>
+     
+
+  
+     
+
+      {/* Short Description */}
+      <p className="text-gray-700 mt-5">{product.shortDescription}</p>
+   
+
+    {/* Sticky Footer for Action Buttons (Mobile Only) */}
+    <div className="p-4 bg-white flex flex-col md:flex-row md:space-x-2 space-y-4 md:space-y-0 md:items-center md:static fixed bottom-0 left-0 right-0 z-10">
+  <Button
+    endContent={<ShoppingCartOutlined />}
+    className="px-6 py-2 text-white hover:opacity-90"
+    style={{
+      backgroundColor: "#FF6A00",
+      color: "#000000",
+      border: "2px solid #000",
+      borderRadius: "4px",
+      transition: "background-color 0.3s, transform 0.2s",
+    }}
+    onMouseEnter={(e) => {
+      e.currentTarget.style.backgroundColor = "#D35400";
+      e.currentTarget.style.transform = "scale(1.05)";
+    }}
+    onMouseLeave={(e) => {
+      e.currentTarget.style.backgroundColor = "#FF6A00";
+      e.currentTarget.style.transform = "scale(1)";
+    }}
+  >
+    <p className="capitalize" style={{ letterSpacing: "0.2em" }}>
+      ADD   TO   CART
+    </p>
+  </Button>
+
+  <Button
+    className="px-6 py-2 text-white"
+    endContent={<HeartTwoTone twoToneColor="#eb2f96" />}
+    style={{
+      backgroundColor: "#c9c6bb",
+      color: "#000000",
+      borderRadius: "4px",
+      border: "none",
+    }}
+  >
+    W h i s h l i s t
+  </Button>
+</div>
+
   </div>
 );
 
