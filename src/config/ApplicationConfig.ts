31031557aabe96f8e0e986f268628c9a7514cb6c
Mohammad yusuf iqbal <yusufiqbal150@gmail.com>
2025-01-30 19:54:18 +0530

    change place order button color in cart

--- src/config/ApplicationConfig.ts
+++ src/config/ApplicationConfig.ts
@@ -1,9 +1,9 @@
 export const  ApplicationConfig ={
     // pulsezest
-    // secretKey: 'Atu7CLUoWoV6knfTr02wX0DXGXt2', 
+    secretKey: 'Atu7CLUoWoV6knfTr02wX0DXGXt2', 
 
     // apnimaati
-    secretKey: 'ykEijsrFiuNimxkgCM3rwmd0C7p2',
+    // secretKey: 'ykEijsrFiuNimxkgCM3rwmd0C7p2',
     applicationName: 'Apni Maati Vastram',
     applicationLogo:  'https://firebasestorage.googleapis.com/v0/b/ecommerce-with-pulsezest.firebasestorage.app/o/app_name%2FykEijsrFiuNimxkgCM3rwmd0C7p2%2FIMG_7577.PNG?alt=media&token=21e8afba-522d-4fa6-a51c-de415cbc0d67',
     baseUrl:  'http://localhost:3000',
