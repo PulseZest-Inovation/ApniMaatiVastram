d5560ecb045b8a0fe4ca12251dedc57b9b1c6f09
Techchauhan <rishabkumarchauhan@gmail.com>
2025-02-07 13:16:58 +0530

    fix build issues

--- src/service/shiprocket/getShipRocketSetting.ts
+++ src/service/shiprocket/getShipRocketSetting.ts
@@ -1,19 +1,18 @@
 import { getDataByDocName } from "../Firebase/getFirestore";
 import { ShipRocketLoginType } from "@/Types/data/ShipRocketLoginType";
 
-export const getShiprocketSetting = async (): Promise<ShipRocketLoginType | null> =>{
+export const getShiprocketSetting = async (): Promise<ShipRocketLoginType | null> => {
     try {
         const data = await getDataByDocName<ShipRocketLoginType>("settings", "shipping");
 
-        if(!data){
-            console.warn("No data fount for Shipp Rocket Setting");
+        if (!data) {
+            console.log("No data found for Shiprocket Setting");
             return null;
         }
 
         return data;
     } catch (error) {
-        console.error("Error getting Shipping Details");
+        console.error("Error getting Shipping Details:", error); // ✅ Log the error
         return null;
-
     }
 };
