ee2ab41379c2f8b0a37760c08d476e2e1f9a42f3
Techchauhan <rishabkumarchauhan@gmail.com>
2025-02-19 23:04:08 +0530

    update the footer

--- src/components/Footer/PopularCategories.tsx
+++ src/components/Footer/PopularCategories.tsx
@@ -1,29 +1,19 @@
+import { ApplicationConfig } from "@/config/ApplicationConfig";
 import Link from "next/link";
 
-type Category = {
-  slug: string;
-  name: string;
-};
-
-type PopularCategoriesProps = {
-  categories: Category[];
-};
-
-const PopularCategories = ({ categories }: PopularCategoriesProps) => {
+ 
+ 
+const PopularCategories = () => {
   return (
     <div className="w-full md:w-1/4">
       <h3 className="text-lg font-semibold mb-4">Popular Categories</h3>
       <ul className="space-y-2">
-        {categories.slice(0, 5).map((category) => (
-          <li key={category.slug}>
-            <Link
-              href={`/collection/${category.slug}`}
-              className="hover:text-blue-400"
-            >
-              {category.name}
-            </Link>
-          </li>
-        ))}
+        <li> <Link href={`${ApplicationConfig.baseUrl}/collection/ready-to-wear-sarees`}>Ready to Wear saree</Link> </li>
+        <li> <Link href={`${ApplicationConfig.baseUrl}/collection/pre-pleated-sarees`}>Pre pleated saree</Link> </li>
+        <li> <Link href={`${ApplicationConfig.baseUrl}/collection/sarees`}>Sarees</Link> </li>
+        <li> <Link href={`${ApplicationConfig.baseUrl}/collection/blouse`}>Blouses </Link> </li>
+        <li> <Link href={`${ApplicationConfig.baseUrl}/collection/earrings`}>Earrings </Link> </li>
+        <li> <Link href={`${ApplicationConfig.baseUrl}/collection/office-wear-saree`}>Office wear Saree </Link> </li>
       </ul>
     </div>
   );
