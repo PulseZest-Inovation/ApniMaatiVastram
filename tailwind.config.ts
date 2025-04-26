46dea64911db3b8f4ad9677d7ba7199ab05cdc9e
techchauhan <rishabkumarchauhan@gmail.com>
2025-04-26 07:30:59 +0530

    update the packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import { nextui } from '@nextui-org/theme';
 import type { Config } from 'tailwindcss';
 
 export default {
-    darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
